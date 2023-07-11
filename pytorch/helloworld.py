089bc29c562b0cae9c2c430d144ab3cdb5228afa
Claudia Misale <34547329+cmisale@users.noreply.github.com>
2023-07-11 13:13:25 -0400

    Update helloworld.py

--- pytorch/helloworld.py
+++ pytorch/helloworld.py
@@ -1,14 +1,21 @@
 import os
 import sys
 import torch
+import torch.distributed as dist
 
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Torch distributed available: ", torch.distributed.is_available())
+    if torch.cuda.is_available():
+        dist.init_process_group("nccl")
+        device = torch.device("cuda")
+    else:
+        dist.init_process_group("gloo")
+        device = torch.device("cpu")
 
     rank = os.environ["RANK"]
     size = os.environ["WORLD_SIZE"]
     node = os.environ["NODE"]
 
     sys.stdout.write(
-    "Hope this code finds you well. Process %s of %s on %s. Device %s. GPU count %d\n"
+    "Hope this code finds you well. Process %s of %s on %s. Device: %s. GPU count: %d\n"
     % (rank, size, node, device, torch.cuda.device_count()))
